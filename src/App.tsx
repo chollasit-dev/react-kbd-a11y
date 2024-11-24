982f94b0d276addecda936f9433800692f6a6d8b
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2024-11-24 21:09:11 +0700

    Customize website fonts, themes, and styles
    - Add self-hosted Thai Google fonts to prevent Google from tracking the users
    - Configure basic dark mode via prefer-color-scheme
    - Add useful Tailwind variant modifiers as an example guide on how to
    extend utility classes
    - Change the name of the <title> tag inside the <head> tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,33 @@
-import '@/App.css';
 import reactLogo from '@/assets/react.svg';
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <section className='h-dvh content-center'>
+        <div className='lg:auto m-auto grid w-fit place-items-center gap-x-8 gap-y-6 px-4 lg:translate-x-8 lg:grid-flow-col lg:px-0'>
+          <div className='lg:-translate-y-2'>
+            <img
+              src={reactLogo}
+              className='size-24 animate-[spin_20s_linear_infinite] lg:size-20'
+              alt='React logo'
+            />
+          </div>
+          <div className='space-y-2 text-pretty text-center text-xl lg:space-y-4 lg:text-start lg:text-2xl'>
+            <h1 className='hidden text-4xl lg:grid lg:text-5xl'>
+              REACT KEYBOARD ACCESSIBILITY
+            </h1>
+            <h1 className='grid text-4xl lg:hidden lg:text-5xl'>
+              REACT KBD A11Y
+            </h1>
+            <div className='max-w-[50ch]'>
+              เว็บไซต์รวบรวม
+              <span className='font-semibold text-cyan-300'> React </span>
+              components รองรับการใช้งานด้วย Keyboard เป็นอย่างดี
+            </div>
+            <div className='max-w-[50ch]'></div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,81 @@
 import { handler as containerQueries } from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import plugin from 'tailwindcss/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['index.html', './src/**/*.{ts,tsx}'],
-  plugins: [containerQueries, forms, typography],
+  darkMode: 'media',
+  plugins: [
+    containerQueries,
+    forms,
+    typography,
+
+    // Variant (pseudo class, element, etc.).
+    plugin(({ addVariant }) => {
+      addVariant('user-valid', '&:user-valid');
+      addVariant('group-user-valid', ':merge(.group):user-valid &');
+      addVariant('peer-user-valid', ':merge(.peer):user-valid ~ &');
+      addVariant('user-invalid', '&:user-invalid');
+      addVariant('group-user-invalid', ':merge(.group):user-invalid &');
+      addVariant('peer-user-invalid', ':merge(.peer):user-invalid ~ &');
+    }),
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['"Noto Sans Thai"', ...defaultTheme.fontFamily.sans],
+        body: ['"Noto Sans Thai Looped"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        card: {
+          DEFAULT: 'var(--card)',
+          foreground: 'var(--card-foreground)',
+        },
+        popover: {
+          DEFAULT: 'var(--popover)',
+          foreground: 'var(--popover-foreground)',
+        },
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+          foreground: 'var(--accent-foreground)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
+        },
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        chart: {
+          1: 'var(--chart-1)',
+          2: 'var(--chart-2)',
+          3: 'var(--chart-3)',
+          4: 'var(--chart-4)',
+          5: 'var(--chart-5)',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
 };
